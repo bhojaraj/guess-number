a5192e5c719684d6d3c0292e3672e4190900f0db
Bharath Gowda <bhojaraj.gouda@gmail.com>
2021-01-09 01:33:48 +0530

    Game logic finished and using expo fonts

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import Header from './components/Header';
+import GameOverScreen from './screens/GameOverScreen';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
 
-  const [ userNumber, setUserNumber ] = useState();
+  const [ userNumber, setUserNumber ] = useState(),
+  [guessRounds, setGuessRounds] = useState(0),
+  [ dataLoaded, setDataLoaded ] = useState(false);
+
+  if( !dataLoaded ){
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => setDataLoaded(true)} 
+        onError={(err) => console.log(err)} 
+      />
+    );
+  }
+
+  const newGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={ startGameHandler } />;
 
-  if( userNumber ) content = <GameScreen userChoice={ userNumber }/>
+  if( userNumber && guessRounds <= 0) {
+    content = <GameScreen userChoice={ userNumber } onGameOver={ gameOverHandler } />;
+  } else if( guessRounds > 0) {
+    content = <GameOverScreen userChoice={ userNumber } roundsNumber={guessRounds} onRestart={newGameHandler} />
+  }
 
   return (
     <View style={ styles.screen } >
